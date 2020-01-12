e8533a47d29a37fb8570aa82f2f6ec846094b208
unheavenlycreature <imaginaryenemy@protonmail.com>
2020-01-12 11:11:10 -0500

    Add flash messages from create and destroy.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,11 +16,17 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
+
+    flash.notice = "Article '#{@article.title}' Deleted!"
+
     @article.destroy
     redirect_to articles_path
   end
