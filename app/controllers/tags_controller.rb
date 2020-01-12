350e11aa976a9aab966ad6268deff231b1e8147e
unheavenlycreature <imaginaryenemy@protonmail.com>
2020-01-12 14:29:53 -0500

    Implement user authentication and also add support for tag deletion.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,20 @@
 class TagsController < ApplicationController
+  before_action :require_login, only: [:destroy]
+
   def index
     @tags = Tag.all
   end
 
   def show
     @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+
+    flash.notice = "Tag '#{@tag.name}' Deleted!"
+
+    @tag.destroy
+    redirect_to tags_path
   end
 end
